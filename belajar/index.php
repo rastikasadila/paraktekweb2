ad78113298e720319a3081adf30524a2ae6cb2d4
rastikasadila <rastika.sadila@gmail.com>
2017-05-03 13:54:41 +0700

    Belajar Include & Require

---
+++ belajar/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Penggabungan</title>
+</head>
+<body>
+	<h1>Judul Halaman</h1>
+	<?php
+	include 'tabel.php';
+	require 'gambar.php';
+	 ?>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+	<?php 
+	include_once 'tabel.php'
+	?>
+</body>
+</html>
---
+++ belajar/tabel.php
@@ -0,0 +1,12 @@
+<table border="1">
+	<tr>
+		<td>No</td>
+		<td>NIM</td>
+		<td>Nama</td>
+	</tr>
+	<tr>
+		<td>1.</td>
+		<td>H1101151027</td>
+		<td>Raras</td>
+	</tr>
+</table>
